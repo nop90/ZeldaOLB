3704a8b9dfedec12d2473f6936745120f16fc2fd
nop90 <nop90nop90@gmail.com>
2016-06-06 19:46:52 +0200

    Starting a new 3DS project

---
+++ src/3ds/os_3ds.cpp
@@ -0,0 +1,98 @@
+
+#include <sys/stat.h>
+#include <sf2d.h>
+#include <sfil.h>
+#include "os_3ds.h"
+#include "sound_3ds.h"
+
+#define TICKS_PER_MSEC (268123.480)
+
+u8 language = 1;
+
+int SDL_Init(int flag) //returns 0 on success, -1 on failure 
+{
+    sf2d_init();
+	sf2d_set_3D(false);
+	sf2d_set_vblank_wait(true);
+	
+	osSetSpeedupEnable(true);
+	
+	romfsInit();
+	cfguInit();
+	soundInit();
+
+	// Read the language field from the config savegame.
+	CFGU_GetSystemLanguage(&language);
+	
+// In case game dir doesn't exist
+    mkdir("/3ds", 0777);
+    mkdir("/3ds/ZeldaOLB", 0777);
+
+    return 0;
+}
+
+
+void SDL_Quit()
+{
+	soundClose();
+	cfguExit();
+    romfsExit();
+	sf2d_fini();
+
+}
+
+int SDL_GetTicks()
+{
+    return svcGetSystemTick()/TICKS_PER_MSEC;
+}
+
+void SDL_Delay(int ms)
+{
+	svcSleepThread(ms*1000000);
+}
+
+// stubbed win functions
+void SDL_WM_SetCaption(const char* caption,void* p) {return;}
+void SDL_WM_SetIcon(void* i, void* p) {return;}
+void SDL_ShowCursor(int s) {return;}
+
+int SDL_PollEvent(int * event)
+{
+	int ret = 0;
+	hidScanInput();
+	*event = hidKeysHeld();
+	if (event) ret= SDL_KEYDOWN;
+	if (*event & KEY_TOUCH) ret|= SDL_FINGERDOWN;
+	if(!aptMainLoop()) ret|= SDL_QUIT;
+	return ret;
+}
+
+int getTouchX(){
+	touchPosition pos;	
+	hidTouchRead(&pos);
+	return pos.px;
+}
+
+int getTouchY(){
+	touchPosition pos;	
+	hidTouchRead(&pos);
+	return pos.py;
+}
+
+int SDL_GetKeyState(void* p)
+{
+	return hidKeysHeld();
+}
+
+int getLanguage(void)
+{
+	return language;
+}
+
+void setLanguage(int languageID)
+{
+	if (languageID>5) language = 1;
+	else if (languageID<1) language = 5;
+	else language = languageID;
+ 
+}
---
+++ src/main.cpp
@@ -0,0 +1,127 @@
+/*
+
+    Zelda Oni Link Begins
+
+    Copyright (C) 2006-2008  Vincent Jouillat
+
+    Please send bugreports with examples or suggestions to www.zeldaroth.fr
+
+*/
+
+#include "3ds/os_3ds.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Keyboard.h"
+#include "Generique.h"
+
+SDL_Surface* init() {             // initialise SDL
+    if(SDL_Init(SDL_INIT_VIDEO) == -1) {
+        exit(-1);
+    }
+    atexit(SDL_Quit);
+
+    SDL_ShowCursor(SDL_DISABLE);
+    
+    return SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE|SDL_DOUBLEBUF|SDL_FULLSCREEN);
+}
+
+int main(int argc, char** argv) {
+    if (argc && argv); //pour éviter un warning.....
+    
+    std::srand(std::time(NULL));
+    
+    SDL_Surface* gpScreen = NULL;
+    
+    int mode = 2; //mode=0;
+    
+    gpScreen = init();
+    
+    Audio* gpAudio = new Audio();
+    Jeu* gpJeu = new Jeu(gpAudio);
+    Carte* gpCarte = new Carte(gpJeu);
+    Encyclopedie* gpEncyclopedie = new Encyclopedie(gpJeu);
+    Keyboard* gpKeyboard = new Keyboard(gpJeu, gpCarte, gpEncyclopedie, gpScreen, mode);
+    gpJeu->setKeyboard(gpKeyboard);
+    Generique* gpGenerique = new Generique(gpJeu);
+    gpJeu->setGenerique(gpGenerique);
+    gpGenerique->initLogo();
+    
+    //gpJeu->init(0);
+    
+    bool gLoop = true;
+    
+    Uint32 lastAnimTime = SDL_GetTicks();
+    
+    while (gLoop && aptMainLoop()) {
+        
+        if (gpKeyboard->gererClavier() == -1) {gLoop = false;}
+        
+        switch (gpKeyboard->getMode()) {
+            case 0 : //jeu normal
+                gpJeu->draw(gpScreen); break;
+            case 3 : //titre
+                gpGenerique->drawTitre(gpScreen); break;
+            case 1 : //disclamer
+            case 2 : //logo
+            case 14 : //générique score
+            case 17 : //menu d'aide 1
+            case 18 : //menu d'aide 2
+                gpGenerique->draw(gpScreen); break;
+            case 4 : //selection
+                gpGenerique->drawSelection(gpScreen, gpKeyboard->getLigne(), 
+                    gpKeyboard->getColonne()); break;
+            case 6 : //options
+                gpGenerique->drawOption(gpScreen, gpKeyboard->getLigneOption(),
+                gpKeyboard->getVolume()/8, gpKeyboard->getVolson()/8); break;
+            case 7 : //charger partie
+                gpGenerique->drawCharger(gpScreen, gpKeyboard->getLigne(), 
+                    gpKeyboard->getLigneVal()); break;
+            case 8 : //générique intro
+                gpGenerique->drawIntro(gpScreen, gpKeyboard->getIntro()); break;
+            case 9 : //effacer partie
+                gpGenerique->drawEffacerSave(gpScreen, gpKeyboard->getLigne(), 
+                    gpKeyboard->getLigneVal()); break;
+            case 10 : //générique début chez link
+                gpGenerique->drawDebut(gpScreen); break;
+            case 11 : //générique fin
+                gpGenerique->drawFin(gpScreen); break;
+            case 12 : //carte
+            case 22 : //carte téléportation
+                gpCarte->draw(gpScreen); break;
+            case 13 : //encyclopédie des monstres
+                gpEncyclopedie->draw(gpScreen); break;
+            case 15 : //records
+            case 19 : //rang 100%
+            case 20 : //rang ultime
+            case 21 : //rang de rapidité
+                gpGenerique->drawRecord(gpScreen, gpKeyboard->getLigneRecord(),
+                    gpKeyboard->getColonneRecord()); break;
+            case 16 : //effacer record
+                gpGenerique->drawEffacer(gpScreen, gpKeyboard->getLigneVal()); break;
+            default : break;
+        }
+        
+        SDL_Flip(gpScreen);
+        
+        if (SDL_GetTicks() < lastAnimTime + 20) SDL_Delay(lastAnimTime+20-SDL_GetTicks());
+        lastAnimTime = SDL_GetTicks();
+    
+    }
+    
+    SDL_FreeSurface(gpScreen); //does nothing
+    
+    delete gpEncyclopedie;
+    delete gpGenerique;
+    delete gpKeyboard;
+    delete gpCarte;
+    delete gpJeu;
+    delete gpAudio;
+    
+    SDL_ShowCursor(SDL_ENABLE);
+    SDL_Quit();
+    exit(0);
+    
+    return 0;
+}
